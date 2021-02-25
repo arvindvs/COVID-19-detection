32bbd7ea937a14a6bd98c6a26705b02b91d24270
Arvind Subramanian <arvinds.99@gmail.com>
2021-02-25 12:17:48 -0800

    Update dataloader

--- dataLoader.py
+++ dataLoader.py
@@ -2,13 +2,14 @@
 
 from __future__ import print_function, division
 import os
+import sys
 import torch
 import pandas as pd
 from skimage import io, transform
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils 
+from torchvision import transforms, utils
 from torchvision import datasets, transforms
 from scipy import misc
 import glob
@@ -16,32 +17,44 @@ import imageio
 from PIL import Image
 
 
-img_dir = "/Users/madhu/Desktop/milestone_images/" 
+# img_dir = "/Users/madhu/Desktop/milestone_images/"
 
-def get_images_from_folder(img_path): 
-	all_images = [] 
-	labels = [] # 0 = covid, 1 = pneumonia, 2 = normal 
-	img_names = os.listdir(img_path)
-	for img in img_names:
-		if 'COVID' in img: 
-			labels.append(0) 
-		elif 'Viral' in img: 
-			labels.append(1) 
-		else: 
-			labels.append(2)
-		full_path = os.path.join(img_path, img)
-		im = imageio.imread(full_path)
-		im_pil = Image.fromarray(im)
+def get_images_from_folder(img_path):
+    all_images = []
+    labels = [] # 0 = covid, 1 = pneumonia, 2 = normal
+    img_names = os.listdir(img_path)
+    for img in img_names:
+        if 'COVID' in img:
+            labels.append(0)
+        elif 'Viral' in img:
+            labels.append(1)
+        else:
+            labels.append(2)
+        full_path = os.path.join(img_path, img)
+        im = imageio.imread(full_path)
+        # im_pil = Image.fromarray(im)
 
-		all_images.append(im_pil) 
-	return all_images, labels
+        all_images.append(im)
+    return all_images, labels, img_names
 
 #################################################################################################
-loaded_dataset, loaded_labels = get_images_from_folder(img_dir)  
-rescaled_images = []
-for image in loaded_dataset:  
-	rescaled = transforms.RandomResizedCrop(256)
-	#(x, y) want to be (256, 256) 
-	#x , y * (y, 256) = x, 256 
-	new_img = rescaled(image)
-	rescaled_images.append(new_img) 
+
+
+def main():
+    # require that the user specify an input and output path
+    assert len(sys.argv) == 3
+    input_dir, output_dir = sys.argv[1:]
+
+    loaded_dataset, loaded_labels, loaded_names = get_images_from_folder(input_dir)
+    rescaled_images = []
+    for i in range(len(loaded_dataset)):
+        img = loaded_dataset[i]
+        new_img = transform.resize(img, (256,256))
+
+        new_img /= 255.0
+        processed_input = torch.tensor(new_img)
+        torch.save(processed_input, os.path.join(output_dir, os.path.splitext(loaded_names[i])[0] + '.pt'))
+
+
+if __name__=="__main__":
+    main()
