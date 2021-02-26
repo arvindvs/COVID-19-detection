2ced1a47cd4f20c78c4d79eaf9ad48543cf71378
Arvind Subramanian <arvinds.99@gmail.com>
2021-02-25 23:24:58 -0800

    Add dataloader and dataset

--- train.py
+++ train.py
@@ -1,45 +1,48 @@
 from models import BaselineFCCOVIDDetector, BaselineConvCOVIDDetector
+from torch.utils.data import Dataset, DataLoader
 import torch
 import torch.optim as optim
 import numpy as np
 from torch import nn
-from dataLoader import get_images_from_folder
+from dataLoader import COVIDDataset
+from torchvision import transforms
 
+csv_file='data/milestone_images/milestone_metadata.csv'
+root_dir='data/milestone_images'
+
+batch_size=1
+img_size=256
+num_epochs = 50
+num_classes = 3
+print_frequency=1
 
 
 def train():
+    dataset = COVIDDataset(csv_file=csv_file, root_dir=root_dir, transform=transforms.Compose([
+                                               transforms.Resize((img_size,img_size)),
+                                               transforms.ToTensor()
+                                           ]))
+    dataloader = DataLoader(dataset, batch_size=batch_size,
+                        shuffle=True, num_workers=0)
+
     model = BaselineConvCOVIDDetector()
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    img_dir = "milestone_images"
 
-    batch_size = 1
-    num_epochs = 50
-    num_steps = 50
-    num_classes = 3
-    
-    imgs, labels, _ = get_images_from_folder(img_dir)
-    m = len(imgs)
     for epoch in range(num_epochs):
-        for i in range(len(imgs)):
-            img = imgs[i]
-            label = labels[i]
-            img = torch.reshape(img, (1, 1, 256, 256))
-            # img shape: 256 x 256
-
-            # torch.rand((batch_size, 3, 256, 256))
-            # labels = torch.tensor(np.random.choice(num_classes, batch_size))
+        for i_batch, sample_batched in enumerate(dataloader):
+            imgs = sample_batched['image'] # shape(batch_size, 1, img_size, img_size)
+            labels = sample_batched['label'] # shape(batch_size,)
 
             optimizer.zero_grad()
-            outputs = model(img)
-            loss = criterion(outputs, label)
+            outputs = model(imgs)
+            loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
 
-            iter_num = epoch*m + i
-            if iter_num % 10 == 9:
-                print(f'iter {iter_num}: loss = {loss}')
 
+            if i_batch % print_frequency == 0:
+                print(f'epoch {epoch}, iter {i_batch}: loss = {loss}')
 
 
 if __name__ == '__main__':
